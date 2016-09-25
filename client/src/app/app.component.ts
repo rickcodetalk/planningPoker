340ce1d061eba255b12d3fad2d130ba9e379b8ca
Rick Lee <eerick903@gmail.com>
2016-09-26 02:01:16 +0800

    Change index.html title and added powered by rickcodetalk link.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
   <div class="container">
     <div class="page-header">
         <h1>Planning Poker</h1>
+        <p>powered by <a href='https://www.youtube.com/channel/UCtEUltuSON06lg_68cwMe4Q' target="_blank">rickcodetalk</a></p>
     </div>
     <router-outlet></router-outlet>
   </div>
